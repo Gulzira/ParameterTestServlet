b1f01aa5566145b46f51ada9309f076056620604
Gulzira <vip.chopi@gmail.com>
2017-11-10 13:32:34 +0600

    Алдынгы срс жанартылды

--- src/kz/kaznitu/su/SessionCalculator.java
+++ src/kz/kaznitu/su/SessionCalculator.java
@@ -7,73 +7,95 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class SessionCalculator extends HttpServlet{
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session =req.getSession(true);
-        Date createTime = new Date (session.getCreationTime());
-        Date lastAccessTime = new Date(session.getLastAccessedTime());
+public class SessionCalculator extends HttpServlet {
+    public static final String SESSION_MAP = "sessionMap";
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
 
-        String title ="Hello,wellcome my friend!";
-        Integer visitCount = new Integer(0);
-        String visitCountKey =new String("visitCount");
-        String userIDKey = new String ("userID");
-        String userID = new String ("ABCD");
 
-        if(session.isNew()) {
-            title = "Welcome to my website";
-            session.setAttribute(userIDKey, userID);
-        } else {
-            visitCount = (Integer) session.getAttribute(visitCountKey);
-            visitCount = visitCount + 1;
-            userID = (String) session.getAttribute(userIDKey);
+        Map<String, List> sessionMap = (Map<String, List>)req.getServletContext().getAttribute("sessionMap");
+
+        if (sessionMap==null){
+            sessionMap = new HashMap<String, List>();
         }
-        session.setAttribute(visitCountKey, visitCount);
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        String docType=
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String title = "Calculator";
+        Double number1 = Double.parseDouble(req.getParameter("number1"));
+        Double number2 = Double.parseDouble(req.getParameter("number2"));
+
+        String oper = req.getParameter("oper");
+
+        HttpSession session = req.getSession(true);
+        String operr = new String();
+        switch(oper) {
+            case "add":
+                oper = (number1 + number2) + "";
+                operr="+";
+                break;
+            case "sub":
+                oper = (number1 - number2) + "";
+                operr="-";
+                break;
+            case "mul":
+                oper = (number1 * number2) + "";
+                operr="*";
+                break;
+            case "div":
+                oper = (number1 / number2) + "";
+                operr="/";
+                break;
+        }
+        ArrayList<String> listOperations;
+
+        if (session.isNew()) {
+            listOperations = new ArrayList<String>();
+        } else {
+            listOperations = (ArrayList<String>) session.getAttribute("formula");
+        }
+        String docType =
                 "<!doctype html>\n";
         out.println(docType +
                 "<html>\n" +
-                "<head><title>" + title + "</title></head>\n"+
+                "<head><title>" + title + "</title></head>\n" +
                 "<body bgcolor = \"#f0f0f0\">\n" +
-                "<hl align = \"center\">" + title + "</h1>\n" +
-                "<h2 align =\"center\">Session Information</h2>\n"+
-                "<table border=\"1\"align=\"center\">\n"+
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n");
 
-                "<tr bgcolor=\"#949494\">\n"+
-                "<th>Session info</th><th>value</th>"+
-                "</tr>\n"+
+        out.println("</td>");
+        out.println("<td style=\"vertical-align:top \">");
+        listOperations.add(number1 + " " + operr + " " + number2 + " = " + oper);
+        session.setAttribute("formula", listOperations);
 
-                "<tr>\n"+
-                "<td>id</td>\n"+
-                "<td>"+session.getId()+"</td>"+
-                "</tr>\n"+
+//out.println("<h1>"+ session.getId() +"</h1>");
 
-                "<tr>\n"+
-                "<td>Creation Time</td>\n"+
-                "<td>"+createTime+"</td>"+
-                "</tr>\n"+
+        for (String opera : listOperations) {
+            out.println("<h3>" + opera + "</h3>");
+        }
 
-                "<tr>\n"+
-                "<td>Time of last Access</td>\n"+
-                "<td>"+lastAccessTime+"</td>"+
-                "</tr>\n"+
-                "<tr>\n"+
+        sessionMap.put(session.getId(), listOperations);
+        getServletContext().setAttribute(SESSION_MAP, sessionMap);
 
-                "<td>user ID</td>\n"+
-                "<td>"+userID+"</td>"+
-                "</tr>\n"+
-                "<tr>\n"+
 
-                "<td>Number of visits</td>\n"+
-                "<td>"+visitCount+"</td>"+
-                "</tr>\n"+
 
-                "<table.\n"+
-                "</body>\n" +
-                "</html>");
-    }
-}
+
+        for (Map.Entry<String, List> entry : sessionMap.entrySet()) {
+            String sessionId = entry.getKey();
+            List listOper = entry.getValue();
+
+// out.println("<h1 style=\"color:red\">" + sessionId + "</h1>");
+
+/*for (Object str : listOper) {
+out.println("<h3>" + str + "</h3>");
+}*/
+            out.println( "</body>\n" +
+                    "</html>"
+            );
+
+        }}}
